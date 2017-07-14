98d79050617b4c7c7de38bb72cee7975b401f00b
Fabricio Machado <fabrizio.machado@tup-take.com>
2017-07-13 23:50:47 -0400

    yikes

--- src/app/detail-dialog/detail-dialog.component.ts
+++ src/app/detail-dialog/detail-dialog.component.ts
@@ -8,6 +8,8 @@ import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 })
 
 export class DetailDialogComponent {
+  editOrAddConfirmationLabel = "Save changes and close";
+  deleteButtonIsVisible = true;
 
   constructor(public dialogRef: MdDialogRef<DetailDialogComponent>, @Inject(MD_DIALOG_DATA) public data: any) { }
 
@@ -17,6 +19,39 @@ export class DetailDialogComponent {
 
   close() {
     this.dialogRef.close();
+  }
+
+  updateScreenForCurrentTab(evt) {
+    if (evt.tab.textLabel === "Add New Relation") {
+      this.deleteButtonIsVisible = false;
+      this.editOrAddConfirmationLabel = "Add relation and close"
+    }
+    if (evt.tab.textLabel === "Edit") {
+      this.deleteButtonIsVisible = true;
+      this.editOrAddConfirmationLabel = "Save changes and close"
+    }
+  }
+
+  saveChangesAndClose() {
+    var job = { action: "", member: "", newrelation:{}};
+    if (this.editOrAddConfirmationLabel === "Add relation and close") {
+      var newMember = { name: "", relationshipttypes: "", relationships: []};
+      job.action = "insert"
+      job.member = this.data
+      job.newrelation = newMember;
+    }
+    if (this.editOrAddConfirmationLabel === "Save changes and close") {
+      job.action = "edit"
+      // TODO: get new values from form and figure out how to indicate that node needs to be updated
+      job.member = this.data
+      job.newrelation = newMember;
+    }
+    this.dialogRef.close(job);
+  }
+
+  deleteMember() {
+    var job = { action: "delete", member: this.data };
+    this.dialogRef.close(job);
   }
 
 }
--- src/app/hierarchy-container/hierarchy-container.component.ts
+++ src/app/hierarchy-container/hierarchy-container.component.ts
@@ -10,7 +10,8 @@ import { DOCUMENT } from '@angular/platform-browser';
   styleUrls: ['./hierarchy-container.component.css']
 })
 export class HierarchyContainerComponent implements OnInit {
-  familyTree = {}
+  familyTree = { data: { name: "" }, children: null }
+  rawFamilyTreeJSON = {}
 
   constructor(public dialog: MdDialog, @Inject(DOCUMENT) doc: any) { }
 
@@ -23,18 +24,42 @@ export class HierarchyContainerComponent implements OnInit {
   }
 
   generateTree(data) {
+    this.rawFamilyTreeJSON = data;
     this.familyTree = d3.hierarchy(data, function (d) {
       return d.relationships;
     })
+    console.log("RAW JSON:");
+    console.log(this.rawFamilyTreeJSON);
+    console.log("D3 TREE:");
+    console.log(this.familyTree);
+
     var treeChart = d3.tree();
-    treeChart.size([400, 400])
+    console.log("TREE CHART:");
+    console.log(treeChart);
+    treeChart.size([400, 400]);
 
     var treeData = treeChart(this.familyTree).descendants()
+    console.log("D3 TREE DESCENDANTS:")
+    console.log(treeData);
     var depthScale = d3.scaleOrdinal().range(["#5EAFC6", "#FE9922", "#93c464", "#75739F"])
     console.log(treeData)
 
     /* Save handle to the main angular context so it can be referenced from within d3 context */
     var angularCtx = this;
+    this.scaffoldTreeVisualization(treeData);
+
+  }
+
+  scaffoldTreeVisualization(treeData) {
+    console.log("D3 TREE DESCENDANTS:")
+    console.log(treeData);
+    var depthScale = d3.scaleOrdinal().range(["#5EAFC6", "#FE9922", "#93c464", "#75739F"])
+    console.log(treeData)
+
+    /* Save handle to the main angular context so it can be referenced from within d3 context */
+    var angularCtx = this;
+
+    //d3.select("#treeG").remove();
 
     d3.select("svg")
       .append("g")
@@ -46,17 +71,20 @@ export class HierarchyContainerComponent implements OnInit {
       .append("g")
       .attr("class", "node")
       .attr("transform", d => "translate(" + d.x + "," + (400 - d.y) + ")")
-    d3.selectAll("g.node")
-      .append("circle")
 
-      /* pass the main angular context into d3 so angular methods can be called from within d3 context */
+    var nodes = d3.selectAll("g.node")
+      // set unique IDs on the node so we can delete them later
+      .attr("id", d => ("node-" + d.data.name))
+      .append("circle")
+      // pass the main angular context into d3 so angular methods can be called from within d3 context 
       .on("click", function (selectedNode) {
         return angularCtx.showDialogForFamilyMember(selectedNode, angularCtx);
       })
       .attr("r", 10)
       .style("fill", d => depthScale(d.depth))
       .style("stroke-width", "2px");
-    d3.select("#treeG").selectAll("line")
+
+    var lines = d3.select("#treeG").selectAll("line")
       .data(treeData.filter(d => d.parent))
       .enter().insert("line", "g")
       .attr("x1", d => d.parent.x)
@@ -64,26 +92,101 @@ export class HierarchyContainerComponent implements OnInit {
       .attr("x2", d => d.x)
       .attr("y2", d => 400 - d.y)
       .style("stroke", "black")
+
+      // set unique IDs on the node so we can delete them later
+      d3.selectAll("line")
+      .attr("id", d => ("line-" + d.data.name))
+
+
+      //lines.exit().remove();
+      //nodes.exit().remove();
+
+      /*d3.select("#treeG").selectAll("line")
+      .data(treeData.filter(d => d.parent)).exit().remove();*/
   }
 
   showDialogForFamilyMember(memberData, context) {
-    console.log(memberData)
-    setTimeout(() => {
-      let dialogRef = this.dialog.open(DetailDialogComponent, memberData);
-      //dialogRef.componentInstance.selectedMember = memberData;
+    console.log(memberData);
+    let dialogRef = this.dialog.open(DetailDialogComponent, memberData);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.action === "delete") {
+        this.deleteFromTree(result, memberData);
+      }
+      if (result.action === "insert") {
 
-      /*
-      dialogRef.afterClosed().subscribe(result => {
-        console.log(result);
-      });
-      */
-    }, 0)
+      }
+      if (memberData.action === "edit") {
+        // TODO
+      }
+    })
   }
+
+  deleteFromTree(member, memberData) {
+    console.log("Deleting in CONTEXT:");
+    console.log(this);
+    console.log("Deleting MEMBER:");
+    console.log(member);
+    console.log("Deleting MEMBER DATA:");
+    console.log(memberData);
+    //this.findAndRemove2(memberData, this.familyTree, 0);
+    this.findAndRemove(memberData, this.rawFamilyTreeJSON, 0);
+  }
+
+  findAndRemove(target, currNode, foundIndex) {
+
+  }
+
+  findAndRemove2(target, currNode, foundIndex) {
+    var numChildren = 0;
+    if (currNode && currNode.children) {
+      numChildren = currNode.children.length;
+    }
+    var idx = 0;
+
+    if (currNode && currNode.data && (target.data.name === currNode.data.name)) {
+      currNode.parent.children.splice(foundIndex, 1);
+      if (currNode.parent.children.length == 0) {
+        /* d3 tree does not accept empty children arrays */
+        currNode.parent.children = null;
+      }
+      var treeChart = d3.tree()
+      var treeData = treeChart(this.familyTree).descendants()
+      var lineID = "#line-" + target.data.name;
+      var nodeID = "#node-" + target.data.name;
+      //d3.select("#treeG").select(lineID).remove()
+      //d3.select(nodeID).remove();
+      //d3.select("#treeG").remove();
+      // this works! d3.select(lineID).remove();
+      //d3.select("svg").html(null);
+      //d3.selectAll("g.node").data(treeData).filter(d => target.data.name === d.data.name).exit().remove();
+      //d3.select(nodeID).remove();
+      //d3.select(lineID).remove();
+      //d3.selectAll("g.node").data(treeData).exit().remove();
+      /*d3.selectAll("g.node").data(treeData).filter(function (d) {
+        return d.data.name === target.data.name;
+      }).exit().remove();*/
+
+      this.scaffoldTreeVisualization(treeData);
+      console.log("found it!");
+      console.log(this.familyTree);
+      return;
+    } else {
+      for (idx = 0; idx < numChildren; idx++) {
+        console.log("still looking...");
+        this.findAndRemove(target, currNode.children[idx], idx);
+      }
+    }
+  }
+
+  insertIntoTree(parent, child) {
+
+  }
+
+
 
   highlightPathToRootMember(memberData) {
     console.log(memberData)
   }
-
 
 
 }
